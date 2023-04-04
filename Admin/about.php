12f82061ce28f0efd4fbdeb2a72343eebedd8449
charisnarizqi <irgienno@gmail.com>
2023-04-04 23:44:24 +0530

    initugas pertemuan 9

---
+++ Admin/about.php
@@ -0,0 +1,15 @@
+<div class="px-4 py-5 my-5 text-center">
+  <img class="rounded d-block mx-auto mb-4">
+  <h1 class="display-5 fw-bold text-body-emphasis">About Me</h1>
+  <div class="col-lg-6 mx-auto">
+    <p class="lead mb-4">Cahrisna Rizqi </p>
+    <p class="lead mb-4">Seee you </p>
+    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+
+    </div>
+
+  </div>
+</div>
+<?php
+include_once 'bottom.php';
+?>
---
+++ Admin/contact.php
@@ -0,0 +1,25 @@
+<div class="px-4 py-5 my-5 text-center">
+  <h3 class="display-5 fw-bold text-body-emphasis">Contact Personal</h3>
+  <div class="col-lg-6 mx-auto">
+    <p class="lead mb-4"> / 089757857966</p>
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Nama</label>
+    </div><br>
+    <form class="form-floating">
+      <input type="email" class="form-control" id="floatingInputValue" placeholder="name@example.com" value="test@example.com">
+      <label for="floatingInputValue">Cantumkan Email Pribadi</label>
+    </form><br>
+    <div class="form-floating">
+      <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+      <label for="floatingTextarea2">Comment / Kepentingan</label>
+    </div><br>
+    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <a href=""><button type="button" class="btn btn-primary btn-lg px-4 gap-3" fdprocessedid="85aqup">Kirim</button></a>
+    </div>
+
+  </div>
+</div>
+<?php
+include_once 'bottom.php';
+?>
